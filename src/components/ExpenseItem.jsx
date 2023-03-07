7312ec921fade3faf306e548aba3c95b1e1b5977
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2023-03-07 14:31:39 -0600

    Build Expenses component

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css';
 
-const ExpenseItem = () => {
+const ExpenseItem = ({ id, title, amount, date }) => {
   return (
-    <div className="expense-item">
-      <div>March 28th 2021</div>
+    <div className="expense-item" id={id}>
+      <ExpenseDate date={date} />
       <div className="expense-item__description">
-        <h2>Car Insurance</h2>
-        <div className="expense-item__price">$294.67</div>
+        <h2>{title}</h2>
+        <div className="expense-item__price">{amount}</div>
       </div>
     </div>
   );
---
+++ src/components/Expenses.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+const Expenses = ({ items }) => {
+  return (
+    <div className="expenses">
+      {items.map((expense) => (
+        <ExpenseItem
+          id={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default Expenses;
