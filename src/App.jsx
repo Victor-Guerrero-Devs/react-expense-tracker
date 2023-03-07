1a3b8bddde476d595470dfdf0061b12cde5fb9ae
Victor-Guerrero-Devs <Vicguerrero.dev@gmail.com>
2023-03-07 14:05:47 -0600

    Build ExpenseItem component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react';
-import './App.css';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
   return (
     <div className="App">
       <h1>Expense Tracker</h1>
+      <ExpenseItem />
     </div>
   );
 }
